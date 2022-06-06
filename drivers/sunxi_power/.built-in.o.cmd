63ab4486d04a226275b9e43fb9fae8f737b04e44
licy <licy.li@myirtech.com>
2022-06-06 19:49:56 +0800

    FEAT: init myd-yt507h uboot

---
+++ drivers/sunxi_power/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/sunxi_power/built-in.o :=  ./../tools/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-ld.bfd     -r -o drivers/sunxi_power/built-in.o drivers/sunxi_power/axp.o drivers/sunxi_power/pmu.o drivers/sunxi_power/pmu_axp858.o drivers/sunxi_power/pmu_axp152.o drivers/sunxi_power/pmu_axp1530.o 
